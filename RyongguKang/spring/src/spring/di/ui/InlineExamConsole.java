3d4d8148ff4cfe4049c88e1736eabe075371138a
RyongguKang <47197156+RyongguKang@users.noreply.github.com>
2020-06-21 22:00:54 +0900

    강룡구, 2주차강의(8~16) 실습예제 커밋
    ㅇㅇ

--- RyongguKang/spring/src/spring/di/ui/InlineExamConsole.java
+++ RyongguKang/spring/src/spring/di/ui/InlineExamConsole.java
@@ -1,26 +1,39 @@
 package spring.di.ui;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
 import spring.di.entity.Exam;
 
+@Component("console")
 public class InlineExamConsole implements ExamConsole {
 	
+	 //순서가 xml파일의 bean을 읽어오고, 그다음 java파일의 Autowired파일이 있는지 확인후, 있으면 해당어노테이션실행, 없으면 bean파일의 property 실행
+	@Autowired
+//	@Qualifier("exam2")
 	private Exam exam;
 	
 	public InlineExamConsole() {
-		// TODO Auto-generated constructor stub
+		System.out.println("constructor");;
 	}
 	
 	public InlineExamConsole(Exam exam) {
+		System.out.println("overload constructor");;
 		this.exam = exam;
 	}
 
 	@Override
 	public void print() {
-		System.out.printf("total is %d, avg is %f\n", exam.total(), exam.avg());
+		if(exam == null)
+			System.out.printf("total is %d, avg is %f\n", 0, 0.0);
+		else
+			System.out.printf("total is %d, avg is %f\n", exam.total(), exam.avg());
 	}
 	
 	@Override
 	public void setExam(Exam exam) {
+		System.out.println("setter constructor");;
 		this.exam = exam;		
 	}
 
